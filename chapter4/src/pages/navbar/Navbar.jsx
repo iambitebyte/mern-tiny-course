6b927c14203f2c5302b61e72eda6547387b9b0ff
chenshuyang <chenshuyang.cn@gmail.com>
2024-01-10 13:12:47 +0800

    typo about css module name.

--- chapter4/src/pages/navbar/Navbar.jsx
+++ chapter4/src/pages/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import styles from './NavBar.module.css';
+import styles from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
 
 export default function Navbar() {
